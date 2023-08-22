25dba3383f74f86cb536f6b04cd37a459f29f7ee
carbonscott <wangimagine@gmail.com>
2023-08-21 21:53:06 -0700

    Add a new argument: context length.  So that it does not need to infer from the input.

--- poorman_transformer/modeling/transformer.py
+++ poorman_transformer/modeling/transformer.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class SingleHeadAttention(nn.Module):
-    def __init__(self, embd_size, head_size, dropout = 0.1):
+    def __init__(self, embd_size, context_length, head_size, dropout = 0.1):
         super().__init__()
 
         self.embd_size = embd_size
@@ -17,7 +17,8 @@ class SingleHeadAttention(nn.Module):
         self.proj_v = nn.Linear(self.embd_size, self.head_size)    # What do I provide to update the entire graph?
 
         # Store a mask to prevent it from gradient tracking...
-        self.mask_initialized = False
+        mask = torch.ones(context_length, context_length).triu(diagonal=1).bool()
+        self.register_buffer('mask', mask)
 
         # Use dropout after the scaled dot self-attention...
         self.dropout = nn.Dropout(dropout)
@@ -39,16 +40,11 @@ class SingleHeadAttention(nn.Module):
         v = self.proj_v(x)
 
         # Scaled dot product...
-        w = q @ k.permute(0,2,1)    # Q @ K.T
+        w = q @ k.permute(0,2,1)    # Q @ K.T -> (B, T, T)
         w /= torch.sqrt(torch.tensor(self.head_size))
 
         # Masking in the decoder...
-        # Store a mask to prevent it from gradient tracking
-        if not self.mask_initialized:
-            mask = torch.ones_like(w).triu(diagonal=1).bool()
-            self.register_buffer('mask', mask)
-            self.mask_initialized = True
-        w[mask] = float('-inf')
+        w[:,self.mask] = float('-inf')    # (B, T, T)
 
         # Obtain the softmax...
         w = w.softmax(dim = -1)
@@ -62,11 +58,11 @@ class SingleHeadAttention(nn.Module):
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, embd_size, head_size, dropout = 0.1):
+    def __init__(self, embd_size, context_length, head_size, dropout = 0.1):
         super().__init__()
 
         num_heads = embd_size // head_size
-        self.multi_head_att_layer = nn.ModuleList([ SingleHeadAttention(embd_size, head_size) for _ in range(num_heads) ])
+        self.multi_head_att_layer = nn.ModuleList([ SingleHeadAttention(embd_size, context_length, head_size) for _ in range(num_heads) ])
 
         self.proj_linear = nn.Linear(embd_size, embd_size)
 
@@ -106,12 +102,12 @@ class FeedForward(nn.Module):
 
 
 class TransformerBlock(nn.Module):
-    def __init__(self, embd_size, num_heads):
+    def __init__(self, embd_size, context_length, num_heads):
         super().__init__()
 
         # Define the multi head attention layer to update node position in a sub space using an attention head...
         head_size = embd_size // num_heads
-        self.multi_head_att_layer = MultiHeadAttention(embd_size, head_size)
+        self.multi_head_att_layer = MultiHeadAttention(embd_size, context_length, head_size)
 
         # Define the feedforward layer to add non-linearity to the model...
         self.ff_layer = FeedForward(embd_size)
@@ -158,35 +154,51 @@ class TransformerBlock(nn.Module):
 
 
 class Transformer(nn.Module):
-    def __init__(self, token_lib_size, embd_size, num_blocks, num_heads):
+    def __init__(self, token_lib_size, embd_size, context_length, num_blocks, num_heads):
         super().__init__()
 
-        # Define the embedding layer to embed each node to a vector space...
-        self.embd_layer = nn.Embedding(token_lib_size, embd_size)
+        # Define token embedding layer to embed each node to a vector space...
+        self.tok_embd_layer = nn.Embedding(token_lib_size, embd_size)
+
+        # Define positional embedding layer to embed each position to a vector space...
+        self.pos_embd_layer = nn.Embedding(context_length, embd_size)
 
         # Define the multi head attention layer to update node position in a sub space using an attention head...
         head_size = embd_size // num_heads
         self.transformer_block = nn.Sequential(*tuple(
-            TransformerBlock(embd_size, num_heads) for _ in range(num_blocks)
+            TransformerBlock(embd_size, context_length, num_heads) for _ in range(num_blocks)
         ))
+
+        # Define layer norm used in the subsequent prediction head...
+        self.layernorm = nn.LayerNorm(embd_size)
 
         # Prediction head...
         self.pred_head = nn.Linear(embd_size, token_lib_size)
 
+        # Store a positional tensor...
+        self.register_buffer('pos_indices', torch.arange(context_length))
+
 
     def forward(self, x):
         """
+        N is number of tokens.
         Arguments:
-            x : (B, T, N)
+            x : (B, T)
         """
         # ___/ EMBED ALL NODES \___
-        nodes_embd = self.embd_layer(x)    # (B, T, N) -> (B, T, E)
+        nodes_tok_embd = self.tok_embd_layer(x)    # (B, T) -> (B, T, E)
+        nodes_pos_embd = self.pos_embd_layer(self.pos_indices)    # (T) -> (T, E)
+
+        nodes_embd = nodes_tok_embd + nodes_pos_embd    # (B, T, E) + (T, E)    =
+                                                        # (B, T, E) + (1, T, E) = (B, T, E)
+
 
         # ___/ MULTI-HEAD ATTENTION BLOCK \___
         # Go through multi-head attention to update nodes in vector space...
         nodes_embd_better = self.transformer_block(nodes_embd)    # (B, T, E) -> (B, T, E)
 
         # ___/ PREDICTION HEAD \___
-        out = self.pred_head(nodes_embd_better)
+        nodes_embd_better_norm = self.layernorm(nodes_embd_better)
+        out = self.pred_head(nodes_embd_better_norm)
 
         return out
